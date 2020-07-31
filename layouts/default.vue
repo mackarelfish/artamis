de9e1ea6e6fdefaff06ae48a280439d22f4ee62d
mackarelfish <mackarelfish93@gmail.com>
2020-07-31 20:06:02 +0700

    Some change

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,13 +20,12 @@
 
     <section class="main-content columns">
       <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
+        <p class="menu-label is-hidden-touch">General</p>
         <ul class="menu-list">
           <li v-for="(item, key) of items" :key="key">
             <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
+              <b-icon :icon="item.icon" />
+              {{ item.title }}
             </nuxt-link>
           </li>
         </ul>
@@ -48,11 +47,6 @@ export default {
           title: 'Home',
           icon: 'home',
           to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
         },
       ],
     }
